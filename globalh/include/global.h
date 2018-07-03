5e4f89f2c128b903264b7e8d988813bc5900c16e
M Hightower <27247790+mhightower83@users.noreply.github.com>
2018-07-03 16:39:17 -0700

    Update global.h

--- globalh/include/global.h
+++ globalh/include/global.h
@@ -1,14 +1,7 @@
 #pragma once
-/**
- * Place in this file, sketch related defines, that the library and core builds
- * need to pickup. These defines should be specific to this
- * sketch. For defines you would like set for all Arduino IDE builds,
- * place those in .../Arduino/include/ide_global.h. Note, an empty default
- * copy of this global.h is also stored there.
+/** 
+ * The following is true, when this file is placed in a sketch's directory:
  *
- * No other defines should be placed here. Only the defines you would like
- * to have in every module that is built for this sketch.
- *
- * For this file to work there are additions that are needed in platform.local.txt.
- *
+ * Defines placed in this sketch's global.h file are unique to this sketch's build. 
+ * And will be used by all library and core modules for that build. 
  */
