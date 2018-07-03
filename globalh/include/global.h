d910db4e59a056eedd115e5e71e246f27321585f
M Hightower <27247790+mhightower83@users.noreply.github.com>
2018-07-02 19:04:06 -0700

    reorg

---
+++ globalh/include/global.h
@@ -0,0 +1,14 @@
+#pragma once
+/**
+ * Place in this file sketch related defines that you would like the library
+ * and core builds to pickup. These defines should be specific to this
+ * sketch. For defines you would like set for all Arduino IDE builds,
+ * place those in .../Arduino/include/ide_global.h. Note, an empty default
+ * copy of this global.h is also stored there.
+ *
+ * No other defines should be placed here. Only the defines you would like
+ * to have in every module that is built for this sketch.
+ *
+ * For this file to work there are additions that are needed in platform.local.txt.
+ *
+ */
---
+++ globalh/include/ide_global.h
@@ -0,0 +1,32 @@
+#ifndef IDE_GLOBAL_H
+#define IDE_GLOBAL_H
+//
+// The Arduino IDE lacks a covenant way to set build defines for a project.
+//
+// This is a work around for that. It requires requires two .h files be
+// available for every build. An ide_global.h file, common for all sketches. It
+// can be empty just needs to exist. Second file, global.h, must exist in
+// every sketch. It too can be empty.
+//
+// Additions to either of these files can be seen by core and library modules.
+// By adding sketch specific defines to sketch/global.h, a library can see those
+// definitions and be built with options specific to a sketchs requirements.
+//
+// Additions of defines to ide_global.h, will effect all modules built.
+//
+// compiler.c.extra_flags=-include "{custom.global.path}\ide_global.h" -include "{build.path}/sketch/global.h"
+// compiler.S.extra_flags=-include "{custom.global.path}\ide_global.h" -include "{build.path}/sketch/global.h"
+// compiler.cpp.extra_flags=-include "{custom.global.path}\ide_global.h" -include "{build.path}/sketch/global.h"
+
+// An example use case, I made changes to "cores/esp8266/Stream.cpp" to
+// skips calls to yield() when (timeout == 0). When timeout was zero, I did
+// not want or expect calls to block and call yieldas Stream was doing in
+// timedRead() and timedPeek(). These changes were made conditinal so that
+// they could be switched off if needed.
+
+// Skips calls to yield() when (timeout == 0)
+// When timeout is zero, we do not want or expect calls to block and
+// call yield as Stream.cpp was doing in timedRead() and timedPeek().
+#define TIMEOUT_ZERO_ASYNC_FIX 1
+
+#endif
