8f34726f17639ab4a9345afbed6965843805fbc0
M Hightower <27247790+mhightower83@users.noreply.github.com>
2018-07-05 14:51:18 -0700

    grammer fixes

--- globalh/include/global.h
+++ globalh/include/global.h
@@ -1,7 +1,9 @@
 #pragma once
-/** 
+/**
  * The following is true, when this file is placed in a sketch's directory:
  *
- * Defines placed in this sketch's global.h file are unique to this sketch's build. 
- * And will be used by all library and core modules for that build. 
+ * Defines, placed in this sketch's global.h file, are unique to this sketch's
+ * build. And will be used by all library and core modules for that build.
+ * To see how to make this work follow link:
+ *   https://github.com/mhightower83/Arduino-IDE-Tweaks/wiki/global-defines
  */
--- globalh/include/ide_global.h
+++ globalh/include/ide_global.h
@@ -2,7 +2,9 @@
 #define IDE_GLOBAL_H
 /*
  * This file lives at ~YourUserId/Arduino/include/ide_global.h
- * Defines placed here are made available to all sketches and modules built.
+ * Defines, placed here, are made available to all sketches and modules built.
+ * To see how to make this work follow link:
+ *   https://github.com/mhightower83/Arduino-IDE-Tweaks/wiki/global-defines
  *
  * Use with Caution.
  *
