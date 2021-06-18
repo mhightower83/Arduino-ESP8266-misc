49e0f6ae5822b6281fab304225ed40d1baf81929
M Hightower <27247790+mhightower83@users.noreply.github.com>
2021-06-18 09:26:17 -0700

    Fixed Wiki reference links

--- globals_h/include/globals.h
+++ globals_h/include/globals.h
@@ -4,7 +4,7 @@
  * Defines, placed in this sketch's globals.h file, are unique to this sketch's
  * build. And will be used by all library and core modules for that build.
  * See link for more information:
- *   https://github.com/mhightower83/Arduino-IDE-Tweaks/wiki/global-defines
+ *   https://github.com/mhightower83/Arduino-ESP8266-misc/wiki/global-defines
  */
 #if (__ASSEMBLER__ == 0)
 
--- globals_h/include/ide_globals.h
+++ globals_h/include/ide_globals.h
@@ -4,11 +4,11 @@
  * This file lives at ~YourUserId/Arduino/include/ide_global.h
  * Defines, placed here, are made available to all sketches and modules built.
  * See link for more information:
- *   https://github.com/mhightower83/Arduino-IDE-Tweaks/wiki/global-defines
+ *   https://github.com/mhightower83/Arduino-ESP8266-misc/wiki/global-defines
  *
  */
 #if (__ASSEMBLER__ == 0)
 
- 
+
 #endif
 #endif
